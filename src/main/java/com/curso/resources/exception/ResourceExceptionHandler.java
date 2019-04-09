ce694c1ba0fb81c576ca5d64c2a0e8a4c57c14a6
letrindade <lucas.eduardo.t.silva@gmail.com>
2019-04-09 14:28:57 -0300

    exceptions de produto/categoria, verificacao de busca de categoria ID

---
+++ src/main/java/com/curso/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.curso.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.curso.service.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	//indica que é um tratador de exceções do tipo da exception da classe 
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+}
